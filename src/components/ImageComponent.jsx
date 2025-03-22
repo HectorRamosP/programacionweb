3683c68a01e9034b93f027ef5e8175bfe5a73db2
HectorRamosP <hetorbeto1@gmail.com>
2025-03-21 18:34:41 -0700

    Cambios

---
+++ src/components/ImageComponent.jsx
@@ -0,0 +1,10 @@
+const ImageComponent = () => {
+    return (
+      <div>
+        <img src="/23.png" alt="Imagen 23" width="200" />
+      </div>
+    );
+  };
+  
+  export default ImageComponent;
+  
