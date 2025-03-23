60c3eeac130f53c914e73082e29b6928c4c13b48
Fernando Molinares <fernandomr.99@hotmail.com>
2025-03-22 22:55:17 -0700

    fear: John china
    Se agrego una foto de john china xd

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,8 +1,10 @@
 const ImageComponent = () => {
     return (
       <div>
-        <img src="/23.png" alt="Imagen 23" width="200" />
+        <img src="/23.png" alt="Imagen 23" width="200" /><br />
+        <img src="/john_china.jpg" alt="Imagen 23" width="200" />
       </div>
+      
     );
   };
   
