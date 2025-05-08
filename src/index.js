464f7be87099de46a8f96cea735139038570a48c
HectorRamosP <hetorbeto1@gmail.com>
2025-05-08 13:39:17 -0700

    Redux
    Implementacion de redux al proyecto

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <Main />
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
